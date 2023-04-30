73d115999de7a0017088f34d25726c7a84fe1b7b
Yash Mittal <112101054@smail.iitpkd.ac.in>
2023-04-30 17:44:41 +0530

    updated to work with frontend

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -30,13 +30,13 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-// const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
-// const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
+const frontEndContractsFile = "../../../tmp/nextjs-smartcontract-lottery-yash/constants/contractAddresses.json"
+const frontEndAbiFile = "../../../tmp/nextjs-smartcontract-lottery-yash/constants/abi.json"
 
 module.exports = {
     networkConfig,
     developmentChains,
     VERIFICATION_BLOCK_CONFIRMATIONS,
-    // frontEndContractsFile,
-    // frontEndAbiFile,
+    frontEndContractsFile,
+    frontEndAbiFile,
 }
